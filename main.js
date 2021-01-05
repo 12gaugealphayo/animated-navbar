5ddeaab0406ed177782c13752a4f9ea2279585a9
12gaugealphayo <css.animation.effects@gmail.com>
2021-01-05 14:57:25 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function sumUp(array) {
+    result = 0;
+    for(n=0; n < array.length; n++){
+        result = result + Number(array[n]);
+    }
+    return result;
+}
+
+a = sumUp([1,12,3,8,7]);
+console.log(a);
+
+
+function sumUp2(array) {
+    return 
+}
